362c2e2382ec598c2137a5daf82d06fac1ccc5ea
dwaraka prakash <dwarakaprakash8210@gmail.com>
2026-04-05 18:23:43 +0530

    fix frontend routing

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,9 @@ const authRoutes = require("./routes/auth");
 const fileRoutes = require("./routes/files");
 
 const app = express();
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "../frontend")));
 
 app.use(cors());
 app.use(express.json());
@@ -13,6 +16,9 @@ app.use("/uploads", express.static("uploads"));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/files", fileRoutes);
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/index.html"));
+});
 
 app.listen(5000, () => {
   console.log("Server running on port 5000");
