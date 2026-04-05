00cf0db2152d395444094fb8a4e49fb9be668845
dwaraka prakash <dwarakaprakash8210@gmail.com>
2026-04-05 18:41:25 +0530

    set login as homepage

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use("/uploads", express.static("uploads"));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/files", fileRoutes);
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/index.html"));
 });
 
