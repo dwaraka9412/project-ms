fc662bff68e03639f67c4e86e227894e2fafb2e3
dwaraka prakash <dwarakaprakash8210@gmail.com>
2026-04-05 19:26:01 +0530

    added redirect to otp page

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,21 @@ const fileRoutes = require("./routes/files");
 
 const app = express();
 const path = require("path");
+const nodemailer = require("nodemailer");
+const session = require("express-session");
+const otpGenerator = require("otp-generator");
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.EMAIL_PASS
+  }
+});
+app.use(session({
+  secret: "msdhoni",
+  resave: false,
+  saveUninitialized: true
+}));
 
 app.use(express.static(path.join(__dirname, "../frontend")));
 
@@ -18,6 +33,42 @@ app.use("/api/auth", authRoutes);
 app.use("/api/files", fileRoutes);
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/index.html"));
+});
+app.post("/register", (req, res) => {
+  const { email, password } = req.body;
+
+  const otp = otpGenerator.generate(6, {
+    upperCase: false,
+    specialChars: false
+  });
+
+  req.session.otp = otp;
+  req.session.user = { email, password };
+
+  const mailOptions = {
+    from: process.env.EMAIL,
+    to: email,
+    subject: "Your OTP Code",
+    text: `Your OTP is ${otp}`
+  };
+
+  transporter.sendMail(mailOptions, (err, info) => {
+    if (err) {
+      return res.send("Error sending OTP");
+    }
+    res.redirect("/otp.html");
+  });
+});
+
+app.post("/verify-otp", (req, res) => {
+  const { otp } = req.body;
+
+  if (otp === req.session.otp) {
+    // Save user (for now just confirm)
+    return res.send("Registration successful");
+  } else {
+    return res.send("Invalid OTP");
+  }
 });
 
 app.listen(5000, () => {
